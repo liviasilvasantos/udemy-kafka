f375c55079b8d47b42596c83f32a1d8fd9cf437c
Livia Silva Santos <liviasilvasantos@gmail.com>
2022-11-30 13:02:26 -0300

    add callback

--- str-producer/src/main/java/com/liviasilvasantos/strproducer/services/StringProducerService.java
+++ str-producer/src/main/java/com/liviasilvasantos/strproducer/services/StringProducerService.java
@@ -1,16 +1,21 @@
 package com.liviasilvasantos.strproducer.services;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class StringProducerService {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
 
-    public void sendMessage(final String message){
-        kafkaTemplate.send("str-topic", message);
+    public void sendMessage(final String message) {
+        kafkaTemplate.send("str-topic", message).addCallback(
+                success -> log.info("message sent with success {} at partition {} for offset {}",
+                        message, success.getRecordMetadata().partition(), success.getRecordMetadata().offset()),
+                failure -> log.error("error sending message"));
     }
 }
