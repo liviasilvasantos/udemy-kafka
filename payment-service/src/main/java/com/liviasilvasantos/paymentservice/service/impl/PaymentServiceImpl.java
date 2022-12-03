09db299179ff3332f03a37f9bb9f6820b90eb1a1
Livia Silva Santos <liviasilvasantos@gmail.com>
2022-12-03 08:38:14 -0300

    add json producer

--- payment-service/src/main/java/com/liviasilvasantos/paymentservice/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/liviasilvasantos/paymentservice/service/impl/PaymentServiceImpl.java
@@ -2,15 +2,28 @@ package com.liviasilvasantos.paymentservice.service.impl;
 
 import com.liviasilvasantos.paymentservice.model.Payment;
 import com.liviasilvasantos.paymentservice.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class PaymentServiceImpl implements PaymentService {
 
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+
+    @SneakyThrows
     @Override
     public void sendPayment(Payment payment) {
-        log.info("sending payment {}", payment);
+        log.info("received payment {}", payment);
+        Thread.sleep(1000);
+
+        log.info("sending payment...");
+        kafkaTemplate.send("payment-topic", payment);
     }
 }
